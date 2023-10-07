4bbd58631481d8d29c0b3b92fd510ff6faa758b6
@Ayham_Hammami <ayhmhamame14@gmail.com>
2023-10-07 20:48:32 +0300

    feat (server/user) : create the user module and its service and resolver Breaking chagnes : new migration for the avatarUrl cloumn

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,35 +1,53 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Context, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
-
-@Resolver(() => User)
+import { Request } from 'express';
+import { UseGuards } from '@nestjs/common';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+@Resolver()
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
+  @UseGuards(AuthGuard)
   @Mutation(() => User)
-  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.userService.create(createUserInput);
+  
+  async updateProfile(
+    @Args('fullname') fullname: string,
+    @Args('file', { type: () => GraphQLUpload, nullable: true })
+    file: GraphQLUpload.FileUpload,
+    @Context() context: { req: Request },
+  ) {
+    const imageUrl = file ? await this.storeImageAndGetUrl(file) : null;
+    const userId = context.req.user.sub;
+    return this.userService.updateProfile(userId, fullname, imageUrl);
   }
 
-  @Query(() => [User], { name: 'user' })
-  findAll() {
-    return this.userService.findAll();
+  private async storeImageAndGetUrl(file: GraphQLUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`;
+    const readStream = createReadStream();
+    readStream.pipe(createWriteStream(imagePath));
+    return imageUrl;
   }
 
-  @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.userService.findOne(id);
+  @UseGuards(AuthGuard)
+  @Query(() => [User])
+  async searchUsers(
+    @Args('fullname') fullname: string,
+    @Context() context: { req: Request },
+  ) {
+    return this.userService.searchUsers(fullname, context.req.user.sub);
   }
 
-  @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.userService.update(updateUserInput.id, updateUserInput);
-  }
-
-  @Mutation(() => User)
-  removeUser(@Args('id', { type: () => Int }) id: number) {
-    return this.userService.remove(id);
+  @UseGuards(AuthGuard)
+  @Query(() => [User])
+  getUsersOfChatroom(@Args('chatroomId') chatroomId: number) {
+    return this.userService.getUsersOfChatroom(chatroomId);
   }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,26 +1,83 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
-
+import * as fs from 'fs';
+import { join } from 'path';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async updateProfile(userId: number, fullname: string, avatarUrl: string) {
+    if (avatarUrl) {
+      const oldUser = await this.prisma.user.findUnique({
+        where: { id: userId },
+      });
+      const updatedUser = await this.prisma.user.update({
+        where: { id: userId },
+        data: {
+          fullname,
+          avatarUrl,
+        },
+      });
+
+      if (oldUser.avatarUrl) {
+        const imageName = oldUser.avatarUrl.split('/').pop();
+        const imagePath = join(
+          __dirname,
+          '..',
+          '..',
+          'public',
+          'images',
+          imageName,
+        );
+        if (fs.existsSync(imagePath)) {
+          fs.unlinkSync(imagePath);
+        }
+      }
+
+      return updatedUser;
+    }
+    return await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        fullname,
+      },
+    });
+  }
+  async searchUsers(fullname: string, userId: number) {
+    // make sure that users are found that contain part of the fullname
+    // and exclude the current user
+    return this.prisma.user.findMany({
+      where: {
+        fullname: {
+          contains: fullname,
+        },
+        id: {
+          not: userId,
+        },
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async getUsersOfChatroom(chatroomId: number) {
+    return this.prisma.user.findMany({
+      where: {
+        chatrooms: {
+          some: {
+            id: chatroomId,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getUser(userId: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
   }
 }
