215fa69457cdbe749181a7d06c56c26d2e588b57
@Ayham_Hammami <ayhmhamame14@gmail.com>
2023-10-09 22:23:18 +0300

    feat (client/src) : add apollo provider and app router to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo/apolloClient.ts";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/chatrooms/:id",
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </React.StrictMode>
+);
